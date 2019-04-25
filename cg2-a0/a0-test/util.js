dac446e3f54f7dd9bafe037cbfc5180e504b466e
arsonite <notarson@gmail.com>
2019-04-25 10:30:39 +0200

    refractoring

--- cg2-a0/a0-test/util.js
+++ cg2-a0/a0-test/util.js
@@ -1,52 +1,51 @@
 /*
  * Collection of useful "global" utility functions.
  * Author: Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
- * (C)opyright 2011-2012 by Hartmut Schirmacher, all rights reserved. 
+ * (C)opyright 2011-2012 by Hartmut Schirmacher, all rights reserved.
  * changes by Martin Puse
  */
 
-/** 
- *  Error object to be thrown for custom runtime errors 
+/**
+ *  Error object to be thrown for custom runtime errors
  */
 let RuntimeError = function(msg, obj) {
-    var err = new Error(msg);
-    err.name = "Runtime error"; 
-    err.msg  = msg || "unknown error";
-    err.obj  = obj;
-    return err;
-}
+  var err = new Error(msg);
+  err.name = 'Runtime error';
+  err.msg = msg || 'unknown error';
+  err.obj = obj;
+  return err;
+};
 
-/** 
+/**
  *   Display the message / object related to a fatal error
  *   and re-throw the error for the browser to act accordingly.
  *   Thx to Henrik Tramberend for an example of this.
  */
 let fatalError = function(err) {
-    // show error on the console
-    window.console.log(err.msg);
+  // show error on the console
+  window.console.log(err.msg);
 
-    // if there is an object associated with the error,
-    // show it on the console for inspection
-    if (err.obj) {
-        window.console.log(err.obj);
-    }
+  // if there is an object associated with the error,
+  // show it on the console for inspection
+  if (err.obj) {
+    window.console.log(err.obj);
+  }
 
-    // rethrow the error for the browser / debugger
-    throw err;  
+  // rethrow the error for the browser / debugger
+  throw err;
 };
-  
-        
+
 /** return the [x,y] position within the HTML canvas element.
     **Please note** that this will only work if the positioning 
     of the canvas element has been set to "relative"!
 */
 let canvasPosition = function(event) {
-    return [event.layerX, event.layerY];
+  return [event.layerX, event.layerY];
 };
-    
+
 /* interface defined by this module */
-export default { 
-    fatalError,
-    canvasPosition,
-    RuntimeError
+export default {
+  fatalError,
+  canvasPosition,
+  RuntimeError
 };
--- cg2-a0/a0-test/vec2.js
+++ cg2-a0/a0-test/vec2.js
@@ -1,12 +1,11 @@
-
 /** 
 	@class Vec2
 	represents a 2D point position or direction, 
 	consists of two public attributes "x" and "y".
 */
 let vec2 = function(x, y) {
-	this.x = x;
-	this.y = y;
-}
+  this.x = x;
+  this.y = y;
+};
 
 export default vec2;
