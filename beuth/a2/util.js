c2a1a718c1ad9b768ea6400287e969b2de92ca8c
arsonite <notarson@gmail.com>
2019-05-09 12:43:47 +0200

    refractoring

--- beuth/a2/util.js
+++ beuth/a2/util.js
@@ -1,53 +1,52 @@
-
 /*
  * Loose collection of helper functions
  */
 let util = {}
 
 // handy shortcut
-util.byid = function(id) {
+util.byid = function (id) {
     return document.getElementById(id)
 }
 
 let _logLevel = 0
-util.setLogLevel = function(level) {
+util.setLogLevel = function (level) {
     _logLevel = level
 }
 
-util.log  = function(msg) { if (_logLevel <= 0) console.log(`%c${msg}`, 'color: grey') }
-util.info = function(msg) { if (_logLevel <= 1) console.log(`%c${msg}`, 'color: green') }
-util.warn = function(msg) { if (_logLevel <= 2) console.log(`%c${msg}`, 'color: orange') }
-util.crit = function(msg) { if (_logLevel <= 3) console.log(`%c${msg}`, 'color: red') }
+util.log = function (msg) { if (_logLevel <= 0) console.log(`%c${msg}`, 'color: grey') }
+util.info = function (msg) { if (_logLevel <= 1) console.log(`%c${msg}`, 'color: green') }
+util.warn = function (msg) { if (_logLevel <= 2) console.log(`%c${msg}`, 'color: orange') }
+util.crit = function (msg) { if (_logLevel <= 3) console.log(`%c${msg}`, 'color: red') }
 
-util.fatal = function(msg) {
+util.fatal = function (msg) {
     throw new Error(msg)
 }
 
-util.isArray = function(arr) {
+util.isArray = function (arr) {
     return Object.prototype.toString.call(arr) === '[object Array]'
 }
 
 // returns a number between min and max
-util.rand = function(min, max) {
+util.rand = function (min, max) {
     return min + (max - min) * Math.random()
 }
 
 // returns an integer between min and max
-util.irand = function(min, max) {
+util.irand = function (min, max) {
     return Math.round(
         min + (max - min) * Math.random()
     )
 }
 
 // helper: convert a byte (0...255) to a 2-digit hex string
-let _byte2hex = function(byte) {
+let _byte2hex = function (byte) {
     let string = byte.toString(16)  // convert to hex string
     return string.length === 1 ?    // eventually pad with leading 0
-        '0' + string : string 
+        '0' + string : string
 }
 
 // generates a random color in hex notation #rrggbb
-util.randRGBHex = function() {
+util.randRGBHex = function () {
     return `# 
         ${_byte2hex(Math.floor(Math.random() * 256))}
         ${_byte2hex(Math.floor(Math.random() * 256))}
@@ -56,7 +55,7 @@ util.randRGBHex = function() {
 }
 
 // generates a random color in byte notation [0-255, 0-255, 0-255]
-util.randRGBByte = function() {
+util.randRGBByte = function () {
     return [
         Math.floor(Math.random() * 256),
         Math.floor(Math.random() * 256),
@@ -65,7 +64,7 @@ util.randRGBByte = function() {
 }
 
 // generates a random color in normalized notation [0-1, 0-1, 0-1]
-util.randRGBNorm = function() {
+util.randRGBNorm = function () {
     return [
         Math.random(),
         Math.random(),
@@ -74,12 +73,12 @@ util.randRGBNorm = function() {
 }
 
 // interpolates between a and b with t from [0, 1]
-util.lerp = function(a, b, t) {
+util.lerp = function (a, b, t) {
     return (1 - t) * a + t * b
 }
 
 // interpolates rgba channels between colora and colorb with t from [0, 1]
-util.lerpRGBA = function(colora, colorb, t) {
+util.lerpRGBA = function (colora, colorb, t) {
     let oot = 1 - t
     return [
         oot * colora[0] + t * colorb[0],
@@ -89,19 +88,18 @@ util.lerpRGBA = function(colora, colorb, t) {
     ]
 }
 
-util.length = function(vec) {
-    return Math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
+util.length = function (vec) {
+    return Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
 }
 
-util.normalize = function(vec) {
-    let len = Math.sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2])
+util.normalize = function (vec) {
+    let len = Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2])
     len = len ? 1.0 / len : 0.0
     return [
-        vec[0] * len, 
-        vec[1] * len, 
+        vec[0] * len,
+        vec[1] * len,
         vec[2] * len
     ]
 }
-
 
 export default util
