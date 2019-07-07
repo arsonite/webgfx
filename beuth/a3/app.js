362cb5bdef97f22ce5ce98ebd0cec200c52a20c4
arsonite <notarson@gmail.com>
2019-07-07 14:08:19 +0200

    added skybox

--- beuth/a3/app.js
+++ beuth/a3/app.js
@@ -4,24 +4,20 @@
  * Modified by Burak Günaydin (853872)
  */
 
+const _ = undefined;
+const texturePath = './textures/';
+
 /* Global configuration */
 let config = {
 	render: {
 		factor: 2,
 		width: window.innerWidth,
 		height: window.innerHeight
-	},
-
-	camera: {
-		fov: 90,
-		ratio: window.innerWidth / window.innerHeight,
-		nearClip: 0.1,
-		farClip: 1000
 	}
 };
 
 /* Setup the renderer */
-let renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(
 	config.render.width * config.render.factor,
 	config.render.height * config.render.factor
@@ -33,28 +29,93 @@ let scene = new THREE.Scene();
 let axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
 
-/* Create a camera */
-let camera = new THREE.PerspectiveCamera(
-	config.camera.fov,
-	config.camera.ratio,
-	config.camera.nearClip,
-	config.camera.farClip
-);
-let radius = 10;
+/* A3.1.1 */
+/*
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+*/
+
+/* A3.1.2 */
+class StellarBody {
+	constructor(
+		name = 'test',
+		extension = 'map',
+		filetype = 'jpg',
+		position = { x: 0, y: 0, z: 0 },
+		type = 'Planet'
+	) {
+		this.model = new THREE.Object3D();
+		this.texture = new THREE.TextureLoader().load(
+			`${texturePath}${name}_${extension}.${filetype}`
+		);
+		this.position;
+	}
+}
+
+/* Light */
+let light = new THREE.PointLight(0xffff00, 1);
+light.castShadow = true;
+
+/* Skybox */
+let textureSky = new THREE.TextureLoader().load(`${texturePath}skybox.jpg`);
+let skyGeometry = new THREE.SphereBufferGeometry(300, 32, 32);
+let skyMaterial = new THREE.MeshBasicMaterial({ map: textureSky });
+skyMaterial.side = THREE.BackSide;
+let sky = new THREE.Mesh(skyGeometry, skyMaterial);
+scene.add(sky);
+
+const stellarBodies = [new StellarBody('earth')];
+
+/* Cameras */
+class Camera {
+	constructor(config) {
+		let exists = (element, defaultValue) =>
+			element !== _ ? element : defaultValue;
+
+		this.fov = exists(config.fov, 90);
+		this.ratio = exists(config.ratio, window.innerWidth / window.innerHeight);
+		this.near = exists(config.near, 0.1);
+		this.far = exists(config.far, 1000);
+
+		this.ortho = exists(config.ortho, false);
+
+		this.position = exists(config.position, { x: 0, y: 5, z: 5 });
+		this.rotation = exists(config.rotation, 0);
+
+		if (this.ortho) {
+			this.inner = new THREE.OrthographicCamera(
+				this.fov,
+				this.ratio,
+				this.near,
+				this.far
+			);
+		} else {
+			this.inner = new THREE.PerspectiveCamera(
+				this.fov,
+				this.ratio,
+				this.near,
+				this.far
+			);
+		}
+		this.inner.position.x = this.position.x;
+		this.inner.position.y = this.position.y;
+		this.inner.position.z = this.position.z;
+	}
+}
+
+let cameras = [
+	new Camera({ rotation: 10 }),
+	new Camera({ rotation: 2 }),
+	new Camera({})
+];
+let cameraIndex = 0;
 
 /* Apply transformation on window resize */
 window.onresize = () => {
 	config.render.width = window.innerWidth;
 	config.render.height = window.innerHeight;
-
-	config.camera.ratio = config.render.width / config.render.height;
-
-	camera = new THREE.PerspectiveCamera(
-		config.camera.fov,
-		config.camera.ratio,
-		config.camera.nearClip,
-		config.camera.farClip
-	);
 };
 
 window.onload = () => {
@@ -62,28 +123,24 @@ window.onload = () => {
 	let delta = 1 / 60;
 	let time = -delta;
 
+	let currentCamera = cameras[cameraIndex];
+
 	let update = function() {
 		time += delta;
 
-		camera.position.x = radius * Math.sin(time);
-		camera.position.z = radius * Math.cos(time);
-		camera.lookAt(new THREE.Vector3(0, 0, 0));
+		if (currentCamera.rotation > 0) {
+			currentCamera.inner.position.x = currentCamera.rotation * Math.sin(time);
+			currentCamera.inner.position.z = currentCamera.rotation * Math.cos(time);
+		}
+		currentCamera.inner.lookAt(new THREE.Vector3(0, 0, 0));
 	};
-
-	/* A3.1.1 */
-	let geometry = new THREE.BoxGeometry(1, 1, 1);
-	let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	let cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
-
-	camera.position.z = 5;
 
 	/* Simulation loop */
 	let render = function() {
 		requestAnimationFrame(render);
 
 		update();
-		renderer.render(scene, camera);
+		renderer.render(scene, currentCamera.inner);
 	};
 
 	render();
