6a8d065f7c10bfaafddfe1eaab8e7d9044139d18
arsonite <notarson@gmail.com>
2019-07-07 11:55:22 +0200

    added a3

--- beuth/a3/app.js
+++ beuth/a3/app.js
@@ -4,6 +4,43 @@
  */
 
 window.onload = function() {
+<<<<<<< HEAD
+	// setup the renderer
+	let renderer; /* TODO */
+
+	// create a scene
+	let scene; /* TODO */
+	let axesHelper = new THREE.AxesHelper(2);
+	scene.add(axesHelper);
+
+	// create a camera
+	let camera; /* TODO */
+	let radius = 10;
+
+	// setup simulation
+	let delta = 1 / 60;
+	let time = -delta;
+
+	let update = function() {
+		time += delta;
+
+		camera.position.x = radius * Math.sin(time);
+		camera.position.z = radius * Math.cos(time);
+		camera.lookAt(new THREE.Vector3(0, 0, 0));
+	};
+
+	// simulation loop
+	let render = function() {
+		requestAnimationFrame(render);
+
+		update();
+		renderer.render(scene, camera);
+	};
+
+	// go
+	render();
+};
+=======
 
     // setup the renderer
     let renderer /* TODO */
@@ -40,3 +77,4 @@ window.onload = function() {
     // go
     render()
 }
+>>>>>>> 576efb8a1e8498fbce68fce23ac6eda9a52b3c80
