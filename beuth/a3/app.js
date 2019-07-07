7a363ae1fed75275c7e126b92d35775d6a2c3d1f
arsonite <notarson@gmail.com>
2019-07-07 13:11:28 +0200

    added resizing functions

--- beuth/a3/app.js
+++ beuth/a3/app.js
@@ -1,23 +1,64 @@
 /*
  * 3DWebGraphics Aufgabe 3
  * (C)opyright Martin Puse, mpuse@beuth-hochschule.de
+ * Modified by Burak Günaydin (853872)
  */
 
-window.onload = function() {
-<<<<<<< HEAD
-	// setup the renderer
-	let renderer; /* TODO */
+/* Global configuration */
+let config = {
+	render: {
+		factor: 2,
+		width: window.innerWidth,
+		height: window.innerHeight
+	},
 
-	// create a scene
-	let scene; /* TODO */
-	let axesHelper = new THREE.AxesHelper(2);
-	scene.add(axesHelper);
+	camera: {
+		fov: 90,
+		ratio: window.innerWidth / window.innerHeight,
+		nearClip: 0.1,
+		farClip: 1000
+	}
+};
 
-	// create a camera
-	let camera; /* TODO */
-	let radius = 10;
+/* Setup the renderer */
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(
+	config.render.width * config.render.factor,
+	config.render.height * config.render.factor
+); // Render size
+document.body.appendChild(renderer.domElement);
 
-	// setup simulation
+/* Create a scene */
+let scene = new THREE.Scene();
+let axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
+/* Create a camera */
+let camera = new THREE.PerspectiveCamera(
+	config.camera.fov,
+	config.camera.ratio,
+	config.camera.nearClip,
+	config.camera.farClip
+);
+let radius = 10;
+
+/* Apply transformation on window resize */
+window.onresize = () => {
+	config.render.width = window.innerWidth;
+	config.render.height = window.innerHeight;
+
+	config.camera.ratio = config.render.width / config.render.height;
+
+	camera = new THREE.PerspectiveCamera(
+		config.camera.fov,
+		config.camera.ratio,
+		config.camera.nearClip,
+		config.camera.farClip
+	);
+};
+
+window.onload = () => {
+	/* Setup simulation */
 	let delta = 1 / 60;
 	let time = -delta;
 
@@ -29,7 +70,15 @@ window.onload = function() {
 		camera.lookAt(new THREE.Vector3(0, 0, 0));
 	};
 
-	// simulation loop
+	/* A3.1.1 */
+	let geometry = new THREE.BoxGeometry(1, 1, 1);
+	let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	let cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+
+	camera.position.z = 5;
+
+	/* Simulation loop */
 	let render = function() {
 		requestAnimationFrame(render);
 
@@ -37,44 +86,5 @@ window.onload = function() {
 		renderer.render(scene, camera);
 	};
 
-	// go
 	render();
 };
-=======
-
-    // setup the renderer
-    let renderer /* TODO */
-
-    // create a scene
-    let scene /* TODO */
-    let axesHelper = new THREE.AxesHelper(2)
-    scene.add(axesHelper)
-
-    // create a camera
-    let camera /* TODO */
-    let radius = 10
-
-    // setup simulation
-    let delta = 1 / 60
-    let time = -delta
-
-    let update = function() {
-        time += delta
-
-        camera.position.x = radius * Math.sin(time)
-        camera.position.z = radius * Math.cos(time)
-        camera.lookAt(new THREE.Vector3(0, 0, 0))
-    }
-
-    // simulation loop
-    let render = function() {
-        requestAnimationFrame(render)
-        
-        update()
-        renderer.render(scene, camera)
-    }
-
-    // go
-    render()
-}
->>>>>>> 576efb8a1e8498fbce68fce23ac6eda9a52b3c80
